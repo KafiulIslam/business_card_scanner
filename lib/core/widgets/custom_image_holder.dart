743a656e00d0990401cd9ecde2e10d89f6b8a11a
KafiulIslam <kafiulraja135@gmail.com>
2025-11-01 16:56:24 +0600

    built netword card

---
+++ lib/core/widgets/custom_image_holder.dart
@@ -0,0 +1,54 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import '../theme/app_colors.dart';
+
+
+class CustomImageHolder extends StatelessWidget {
+  final String imageUrl;
+  final double height;
+  final double width;
+  final bool isCircle;
+  final Widget errorWidget;
+
+  const CustomImageHolder(
+      {super.key,
+        required this.imageUrl,
+        required this.height,
+        required this.width,
+        this.isCircle = true,
+        required this.errorWidget});
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      imageBuilder: (context, imageProvider) => Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: AppColors.primaryLight,
+          shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+          borderRadius: isCircle ? null : BorderRadius.circular(8),
+          image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+        ),
+      ),
+      placeholder: (context, url) => const Padding(
+        padding: EdgeInsets.all(6.0),
+        child: CircularProgressIndicator(
+          color: AppColors.primary,
+        ),
+      ),
+      errorWidget: (context, url, error) => Container(
+        height: height,
+        width: width,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: AppColors.primaryLight,
+          shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+          border: Border.all(color: AppColors.borderColor),
+        ),
+        child: errorWidget,
+      ),
+    );
+  }
+}
--- lib/features/network/presentation/widgets/network_card_list_item.dart
+++ lib/features/network/presentation/widgets/network_card_list_item.dart
@@ -1,6 +1,7 @@
 import 'package:business_card_scanner/core/theme/app_colors.dart';
 import 'package:business_card_scanner/core/theme/app_dimensions.dart';
 import 'package:business_card_scanner/core/theme/app_text_style.dart';
+import 'package:business_card_scanner/core/widgets/custom_image_holder.dart';
 import 'package:business_card_scanner/features/network/domain/entities/network_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -54,9 +55,8 @@ class NetworkCardListItem extends StatelessWidget {
     return InkWell(
       onTap: onTap,
       child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppDimensions.spacing16,
-          vertical: AppDimensions.spacing12,
+        padding: EdgeInsets.all(
+          AppDimensions.spacing12,
         ),
         decoration: BoxDecoration(
             color: AppColors.surface,
@@ -71,151 +71,99 @@ class NetworkCardListItem extends StatelessWidget {
             ]),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             // Business Card Thumbnail
-            Container(
-              width: 80.w,
-              height: 100.h,
-              decoration: BoxDecoration(
-                color: AppColors.gray100,
-                borderRadius: BorderRadius.circular(AppDimensions.radius12),
-                border: Border.all(color: AppColors.borderColor),
-              ),
-              child: card.imageUrl.isNotEmpty
-                  ? ClipRRect(
-                      borderRadius:
-                          BorderRadius.circular(AppDimensions.radius12),
-                      child: Image.network(
-                        card.imageUrl,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) =>
-                            _buildCardPlaceholder(),
-                      ),
-                    )
-                  : _buildCardPlaceholder(),
-            ),
-            Gap(AppDimensions.spacing12),
-
-            // Card Information
+            CustomImageHolder(
+                //imageUrl: card.imageUrl,
+                isCircle: false,
+                imageUrl:
+                    'https://asset.gecdesigns.com/img/visiting-card-templates/bold-and-creative-business-card-template-for-entrepreneurs-1680966036989-cover.webp',
+                height: 90.h,
+                width: 90.w,
+                errorWidget: const Icon(
+                  Icons.contact_mail_sharp,
+                  color: AppColors.iconColor,
+                )),
+            Gap(AppDimensions.spacing8),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   // Name and Category Tag (General)
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          card.name,
-                          style: AppTextStyles.headline4.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
+                  Row(children: [
+                    Expanded(
+                      child: Text(
+                        card.name,
+                        style: AppTextStyles.bodySmall.copyWith(
+                            fontWeight: FontWeight.bold, color: Colors.black),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      if (categories.isNotEmpty)
-                        Container(
-                          margin: EdgeInsets.only(left: AppDimensions.spacing8),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: AppDimensions.spacing8,
-                            vertical: AppDimensions.spacing4,
-                          ),
-                          decoration: BoxDecoration(
-                            color: AppColors.secondaryLight.withOpacity(0.2),
-                            borderRadius:
-                                BorderRadius.circular(AppDimensions.radius8),
-                          ),
-                          child: Text(
-                            categories.first,
-                            style: AppTextStyles.labelSmall.copyWith(
-                              color: AppColors.secondary,
-                              fontSize: 11.sp,
-                            ),
-                          ),
-                        ),
-                    ],
-                  ),
-                  SizedBox(height: AppDimensions.spacing4),
+                    ),
+                    Icon(
+                      Icons.more_horiz,
+                      color: AppColors.primary,
+                      size: 18.w,
+                    )
+                  ],),
+const Gap(2),
                   // Title
                   if (card.title.isNotEmpty)
                     Text(
                       card.title,
-                      style: AppTextStyles.bodyMedium.copyWith(
-                        fontSize: 14.sp,
-                      ),
+                      style: AppTextStyles.labelSmall,
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
                     ),
-                  SizedBox(height: AppDimensions.spacing8),
-                  // Category Tags (Clients, Prospects, etc.)
-                  if (categories.length > 1)
-                    Wrap(
-                      spacing: AppDimensions.spacing8,
-                      runSpacing: AppDimensions.spacing4,
-                      children: categories.skip(1).take(2).map((category) {
-                        return Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: AppDimensions.spacing8,
-                            vertical: AppDimensions.spacing4,
-                          ),
-                          decoration: BoxDecoration(
-                            color: AppColors.secondaryLight.withOpacity(0.2),
-                            borderRadius:
-                                BorderRadius.circular(AppDimensions.radius8),
-                          ),
-                          child: Text(
-                            category,
-                            style: AppTextStyles.labelSmall.copyWith(
-                              color: AppColors.secondary,
-                              fontSize: 11.sp,
-                            ),
-                          ),
-                        );
-                      }).toList(),
+                  const Gap(2),
+                  //Company
+                  if (card.company.isNotEmpty)
+                    Text(
+                      card.company,
+                      style: AppTextStyles.labelSmall,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  SizedBox(height: AppDimensions.spacing8),
-                  // Date with Calendar Icon
-                  if (dateText.isNotEmpty)
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.calendar_today_outlined,
-                          size: 14.w,
-                          color: AppColors.gray600,
-                        ),
-                        SizedBox(width: AppDimensions.spacing4),
-                        Text(
-                          dateText,
-                          style: AppTextStyles.bodySmall.copyWith(
-                            color: AppColors.gray600,
-                            fontSize: 12.sp,
-                          ),
-                        ),
-                      ],
-                    ),
+                  const Gap(2),
+                  Row(children: [
+                 Icon(
+                   Icons.calendar_today_outlined,
+                   size: 14.w,
+                   color: AppColors.gray600,
+                 ),
+                 Gap(AppDimensions.spacing4),
+                 Text(
+                   'dateText',
+                   style: AppTextStyles.bodySmall.copyWith(
+                     color: AppColors.gray600,
+                     fontSize: 12.sp,
+                   ),
+                 ),
+                 const Spacer(),
+                 Container(
+                   margin: EdgeInsets.only(left: AppDimensions.spacing8),
+                   padding: EdgeInsets.symmetric(
+                     horizontal: AppDimensions.spacing8,
+                     vertical: AppDimensions.spacing4,
+                   ),
+                   decoration: BoxDecoration(
+                     color: AppColors.primaryLight.withOpacity(0.2),
+                     borderRadius:
+                     BorderRadius.circular(AppDimensions.radius8),
+                   ),
+                   child: Text(
+                     categories.first,
+                     style: AppTextStyles.labelSmall.copyWith(
+                       color: AppColors.primary,
+                       fontSize: 11.sp,
+                     ),
+                   ),
+                 )
+               ],),
+                  
                 ],
               ),
-            ),
-            Gap(AppDimensions.spacing12),
-            // More Options Icon
-            IconButton(
-              onPressed: onMoreTap,
-              icon: Container(
-                width: 32.w,
-                height: 32.w,
-                decoration: BoxDecoration(
-                  color: AppColors.secondary,
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(
-                  Icons.more_vert,
-                  color: Colors.white,
-                  size: 18.w,
-                ),
-              ),
-              padding: EdgeInsets.zero,
-              constraints: BoxConstraints(),
             ),
           ],
         ),
