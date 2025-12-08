3e4d118dc4791c273a23507ef07c9436df205347
KafiulIslam <kafiulraja135@gmail.com>
2025-12-08 12:34:06 +0600

    implement terms & conditions in signup

--- lib/features/auth/presentation/views/signUp/sign_up_screen.dart
+++ lib/features/auth/presentation/views/signUp/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:business_card_scanner/core/theme/app_dimensions.dart';
 import 'package:business_card_scanner/core/utils/custom_snack.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
@@ -23,6 +24,8 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
+
+  late bool isTermAccepted = false;
 
   @override
   void dispose() {
@@ -70,21 +73,27 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       hintText: 'Enter the password',
                     ),
                     Gap(AppDimensions.spacing24),
+                    _buildTermsCheckBox(),
+                    Gap(AppDimensions.spacing24),
                     PrimaryButton(
                       onTap: () async {
+                        if (isTermAccepted) {
+                          //check and make sure email and password field is not empty
+                          if (_email.text.isEmpty || _password.text.isEmpty) {
+                            CustomSnack.warning(
+                                'Please, enter email and password', context);
+                            return;
+                          }
 
-                        //check and make sure email and password field is not empty
-                        if (_email.text.isEmpty || _password.text.isEmpty) {
+                          //call signup function
+                          context.read<SignupCubit>().signUp(
+                                _email.text.trim(),
+                                _password.text.trim(),
+                              );
+                        } else {
                           CustomSnack.warning(
-                              'Please, enter email and password', context);
-                          return;
+                              'Please accept the Terms & Conditions.', context);
                         }
-
-                        //call signup function
-                        context.read<SignupCubit>().signUp(
-                              _email.text.trim(),
-                              _password.text.trim(),
-                            );
                       },
                       buttonTitle: 'Sign Up',
                       isLoading: isLoading,
@@ -114,4 +123,56 @@ class _SignUpScreenState extends State<SignUpScreen> {
     );
   }
 
+  Widget _buildTermsCheckBox() {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isTermAccepted = !isTermAccepted;
+        });
+      },
+      splashColor: Colors.transparent,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+                color: isTermAccepted ? AppColors.primary : Colors.transparent,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                    color: isTermAccepted
+                        ? Colors.transparent
+                        : AppColors.borderColor,
+                    width: 1.5)),
+            child: const Icon(
+              Icons.check,
+              color: Colors.white,
+              size: 12,
+            ),
+          ),
+          Gap(AppDimensions.spacing8),
+          RichText(
+            text: TextSpan(
+              text: 'I accept the ',
+              style: AppTextStyles.bodyMedium.copyWith(fontSize: 14),
+              children: <TextSpan>[
+                TextSpan(
+                    text: 'Terms & Conditions',
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        context.push(Routes.termsConditions);
+                      },
+                    style: AppTextStyles.headline3.copyWith(
+                        fontSize: 14,
+                        color: AppColors.primary,
+                        decoration: TextDecoration.underline,
+                        decorationColor: AppColors.primary)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
